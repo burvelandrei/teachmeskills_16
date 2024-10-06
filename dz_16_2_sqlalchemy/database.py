21e6f2065238b27cc06d769b41a7ebca89067a92
Andrei Burvel <burvelandrei@gmail.com>
2024-10-07 02:49:47 +0300

    добавил комментарии, убрал удаление таблиц в create_tables

--- dz_16_2_sqlalchemy/database.py
+++ dz_16_2_sqlalchemy/database.py
@@ -3,17 +3,21 @@ from sqlalchemy.orm import sessionmaker
 from config_db import DB, load_config
 from models import Base
 
+# Загружаем параметры базы данных
 config_db: DB = load_config()
 
 
+# Создание движка для базы данных
 engine = create_engine(
-        f"""postgresql+psycopg2://{config_db.db_user}:{config_db.db_password}@{config_db.db_host}:{config_db.db_port}/{config_db.db_name}""",
-        echo=True,
-        pool_recycle=2000,
-    )
+    f"""postgresql+psycopg2://{config_db.db_user}:{config_db.db_password}@{config_db.db_host}:{config_db.db_port}/{config_db.db_name}""",
+    echo=False,
+    pool_recycle=2000,
+)
+
 
 def create_tables():
-    # Base.metadata.drop_all(engine)
-    # engine.echo = True
+    """
+    Функция для создания всех таблиц.
+    """
     Base.metadata.create_all(engine)
-    engine.echo = True
+    engine.echo = False
