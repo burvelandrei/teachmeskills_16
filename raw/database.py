ad09a8f2fc33162f01b2562242e2d0cd56e4c69d
Andrei Burvel <burvelandrei@gmail.com>
2024-10-10 02:29:55 +0300

    V 1.9 обновил подключение через класс

--- raw/database.py
+++ raw/database.py
@@ -1,34 +1,65 @@
 import psycopg2
 from config_db import load_config, DB
 
+
 config_db: DB = load_config()
 
 
-def db_connection(func):
+class DBConnect:
     """
-    Функция декоратор для создания подключения к БД.
+    Класс для создания подключения к БД.
     """
-    try:
-        db_connect = psycopg2.connect(
-            dbname=config_db.db_name,
-            host=config_db.db_host,
-            user=config_db.db_user,
-            password=config_db.db_password,
-            port=config_db.db_port,
+    connection = None
+    def __init__(self, host: str, dbname: str, user: str, password: str, port: int):
+        self.host = host
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.port = port
+
+    def _create_db_connection(self):
+        self.connection = psycopg2.connect(
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
             keepalives=1,
             keepalives_idle=30,
             keepalives_interval=10,
             keepalives_count=5,
         )
-        db_connect.autocommit = True
-        def wrapper(*args, **kwargs):
+        self.connection.autocommit = True
+        return self.connection
+
+    def get_connection(self):
+        if self.connection and not self.connection.closed:
             try:
-                with db_connect as conn:
-                    with conn.cursor() as cur:
-                        result = func(cur, *args, **kwargs)
-                        return result
-            except Exception as e:
-                print(f"Не смог выполнить функцию - {e}")
-        return wrapper
-    except Exception as e:
-        print(f"Не смог подключится к БД - {e}")
+                with self.connection.cursor() as cursor:
+                    cursor.execute("SELECT 1")
+            except psycopg2.OperationalError:
+                return self._create_db_connection()
+            return self.connection
+        else:
+            return self._create_db_connection()
+
+
+db = DBConnect(
+    config_db.db_host,
+    config_db.db_name,
+    config_db.db_user,
+    config_db.db_password,
+    config_db.db_port,
+)
+
+
+def db_connection(func):
+    """
+    Функция декоратор для передачи курсора
+    """
+
+    def wrapper(*args, **kwargs):
+        with db.get_connection().cursor() as cur:
+            return func(cur, *args, **kwargs)
+
+    return wrapper
